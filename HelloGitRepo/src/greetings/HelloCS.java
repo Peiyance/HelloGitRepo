c6334832754284ecde5b08c702bbf623dc1c248c
Peiyance2 <peiyance2@gmail.com>
2022-11-08 15:45:47 +0800

    add a print output

--- HelloGitRepo/src/greetings/HelloCS.java
+++ HelloGitRepo/src/greetings/HelloCS.java
@@ -4,11 +4,12 @@ public class HelloCS {
 	
 	public static void main(String[] args) {
 		  
-		  String[] names = { "Java", "Javascript", "Python", "C#", "HTML" };
+		  String[] names = { "Java", "Javascript", "Python", "C#", "HTML", "PHP", "Ruby"};
 		  
 		  //print greetings
 		  for (int i = 0; i < names.length; i++) {
 		   System.out.format("Hello, %s!\n", names[i]);
+		   System.out.println("How are you going today?");
 		  }
 		  //it should be okay
 	}
