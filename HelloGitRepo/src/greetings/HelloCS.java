ca6d3ba0fc8e2e1d428295df77cb38ca8fdb665e
Peiyance <peiyance@gmail.com>
2022-11-08 12:30:59 +0800

    First Commit

---
+++ HelloGitRepo/src/greetings/HelloCS.java
@@ -0,0 +1,14 @@
+package greetings;
+
+public class HelloCS {
+	
+	public static void main(String[] args) {
+		  
+		  String[] names = { "Java", "Javascript", "Python", "C#", "HTML" };
+		  
+		  //print greetings
+		  for (int i = 0; i < names.length; i++) {
+		   System.out.format("Hello, %s!\n", names[i]);
+		  }
+	}
+}
