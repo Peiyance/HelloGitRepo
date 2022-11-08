0a26f2191314f6862208947edabcadc8735880a5
Peiyance <peiyance@gmail.com>
2022-11-08 15:59:22 +0800

    first commit

--- HelloGitRepo/src/greetings/HelloCS.java
+++ HelloGitRepo/src/greetings/HelloCS.java
@@ -2,17 +2,13 @@ package greetings;
 
 public class HelloCS {
 	
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 		  
-		  String[] names = { "Java", "Javascript", "Python", "C#", "HTML", "PHP", "Ruby"};
+		  String[] names = { "Java", "Javascript", "Python", "C#", "HTML" };
 		  
 		  //print greetings
-
-		  for (String name: names) {
-		   System.out.format("Hello, %s!\n", name);
-		   System.out.println("How are you going today?");
-		   
-		  //it should be okay
+		  for (int i = 0; i < names.length; i++) {
+		   System.out.format("Hello, %s!\n", names[i]);
+		  }
 	}
-}
 }
