111ff48aede9a9e56c38dedb97592527353f3a41
Peiyance <peiyance@gmail.com>
2022-11-08 15:41:54 +0800

    add a loop

--- HelloGitRepo/src/greetings/HelloCS.java
+++ HelloGitRepo/src/greetings/HelloCS.java
@@ -7,8 +7,8 @@ public class HelloCS {
 		  String[] names = { "Java", "Javascript", "Python", "C#", "HTML" };
 		  
 		  //print greetings
-		  for (int i = 0; i < names.length; i++) {
-		   System.out.format("Hello, %s!\n", names[i]);
+		  for (String name: names) {
+		   System.out.format("Hello, %s!\n", name);
 		  }
 		  //it should be okay
 	}
